5e3baa7d3c3627f7e0d5857cdfaa54efec49f477
kuchichan <pawel.kucharski@hotmail.com>
2023-10-31 11:21:24 +0100

    add experiments

--- main.go
+++ main.go
@@ -1,13 +1,46 @@
 package main
 
 import (
-	"fmt"
+	"html/template"
 	"log"
 	"net/http"
 )
 
+type TemplateData struct {
+	Hello       string
+	StockAssets *[]StockAsset
+}
+
+type StockAsset struct {
+	ID       int
+	Ticker   string
+	Quantity int // for now
+	Price    int
+}
+
 func hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, there!</h1>")
+	pattern := []string{
+		"./ui/html/base.tmpl.html",
+		"./ui/html/pages/home.tmpl.html",
+	}
+	ts, err := template.ParseFiles(pattern...)
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server error", http.StatusInternalServerError)
+		return
+	}
+
+	stockAssets := &[]StockAsset{
+		{ID: 1, Ticker: "AAPL", Price: 2990, Quantity: 100},
+		{ID: 2, Ticker: "TSLA", Price: 19901, Quantity: 100},
+		{ID: 3, Ticker: "JPM", Price: 19190, Quantity: 100},
+	}
+
+	err = ts.ExecuteTemplate(w, "base", &TemplateData{Hello: "hehe", StockAssets: stockAssets})
+	if err != nil {
+		log.Print(err.Error())
+		http.Error(w, "Internal Server error", http.StatusInternalServerError)
+	}
 }
 
 func main() {
